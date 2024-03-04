2abd33234d340bd6ec506ab15e40dd952b9f91b0
SoSeDiK <mrsosedik@gmail.com>
2024-03-04 21:23:09 +0200

    help. (does it work?)

--- system/profile/lappytoppy.nix
+++ system/profile/lappytoppy.nix
@@ -14,6 +14,16 @@ let inherit (import ../../profiles/${profileName}/options.nix) homeDir; in
   services.fstrim.enable = true;
   services.teamviewer.enable = true;
 
+  services.openvpn.servers = {
+    zaborona = {
+      config = builtins.readFile (pkgs.fetchurl {
+        url = "https://zaborona.help/openvpn-client-config/srv0.zaborona-help_maxroutes.ovpn";
+        sha256 = "8b3f7d06bf7d55dfae4499b87dff3106517b648d74a5ae5ab977f4b5a164241c";
+      });
+      autoStart = true;
+    };
+  };
+
   # Mount data disk
   fileSystems."${homeDir}/Data" = {
     device = "/dev/sda1";
