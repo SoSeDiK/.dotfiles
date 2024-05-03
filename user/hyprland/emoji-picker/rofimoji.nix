44a57d79ae3f076ce3188df10a957f4b9492856d
SoSeDiK <mrsosedik@gmail.com>
2024-05-03 13:08:47 +0300

    Update rofimoji.nix

--- user/hyprland/emoji-picker/rofimoji.nix
+++ user/hyprland/emoji-picker/rofimoji.nix
@@ -1,9 +1,12 @@
-{ config, pkgs, ... }:
+{ config, pkgs, profileName, ... }:
 
+let
+  inherit (import ../../../profiles/${profileName}/options.nix) homeDir flakeDir;
+in
 {
   home.packages = with pkgs; [
     rofimoji
   ];
 
-  home.file.".local/share/rofimoji/data/emojis_smileys_emotion.additional.csv".source = ./extras/emojis_smileys_emotion.additional.csv;
+  home.file.".local/share/rofimoji/data/emojis_smileys_emotion.additional.csv".source = config.lib.file.mkOutOfStoreSymlink "${flakeDir}/user/hyprland/emoji-picker/extras/emojis_smileys_emotion.additional.csv";
 }
