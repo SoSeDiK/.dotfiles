dfe61bb61ed5efe8c5521cf680ba0e99efcd013e
SoSeDiK <mrsosedik@gmail.com>
2024-05-15 05:12:00 +0300

    UP

--- user/hyprland/default.nix
+++ user/hyprland/default.nix
@@ -30,9 +30,9 @@ in
       source = ${flakeDir}/user/hyprland/hypr/hyprland.conf
     '';
     plugins = [
-      # inputs.hyprland-plugins.packages.${pkgs.system}.hyprexpo
-      # inputs.hyprland-plugins.packages.${pkgs.system}.hyprbars
-      # inputs.hyprsplit.packages.${pkgs.system}.hyprsplit
+      inputs.hyprland-plugins.packages.${pkgs.system}.hyprexpo
+      inputs.hyprland-plugins.packages.${pkgs.system}.hyprbars
+      inputs.hyprsplit.packages.${pkgs.system}.hyprsplit
       # inputs.hyprspace.packages.${pkgs.system}.Hyprspace
     ];
   };
