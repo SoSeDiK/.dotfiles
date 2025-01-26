07f4cd7e973e898481732e9c2e16d8ce5866cd4f
SoSeDiK <mrsosedik@gmail.com>
2025-01-26 14:46:22 +0200

    805 current  2025-01-26 14:44:51  25.05.20250124.825479c  6.12.9-xanmod1                          *

--- system/wm/hyprland/hyprland.nix
+++ system/wm/hyprland/hyprland.nix
@@ -35,9 +35,9 @@ in {
       source = ${dotAssetsDir}/hypr/hyprland.conf
     '';
     plugins = [
-      # inputs'.hyprland-plugins.packages.hyprbars
-      # inputs'.hyprland-plugins.packages.hyprexpo
-      # inputs'.hypr-dynamic-cursors.packages.hypr-dynamic-cursors
+      inputs'.hyprland-plugins.packages.hyprbars
+      inputs'.hyprland-plugins.packages.hyprexpo
+      inputs'.hypr-dynamic-cursors.packages.hypr-dynamic-cursors
       inputs'.hyprsplit.packages.hyprsplit
     ];
   };
