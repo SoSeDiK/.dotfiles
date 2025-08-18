65c5eb5201370135d36e1e30110e1beb479021ed
SoSeDiK <mrsosedik@gmail.com>
2025-08-18 22:04:16 +0300

    134         2025-08-18 21:59:21  25.11.20250814.fbcf476  6.15.9-xanmod1  Unknown                 []              True

--- modules/system/misc/keyring.nix
+++ modules/system/misc/keyring.nix
@@ -1,11 +1,11 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 
 {
   # Keeping passwords / sessions
   services.gnome.gnome-keyring.enable = true;
 
-  # Keyrind data visualizer
-  programs.seahorse.enable = true;
+  # Keyring data visualizer
+  programs.seahorse.enable = lib.mkDefault true;
 
   # Storing app settings
   programs.dconf.enable = true;
