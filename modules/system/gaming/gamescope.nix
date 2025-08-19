d6928e163f59609794bd272cbbb8dea3459d70fe
SoSeDiK <mrsosedik@gmail.com>
2025-08-19 13:58:00 +0300

    139         2025-08-19 12:49:09  25.11.20250814.fbcf476  6.15.9-xanmod1  Unknown                 []              True

--- modules/system/gaming/gamescope.nix
+++ modules/system/gaming/gamescope.nix
@@ -1,8 +1,20 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs.gamescope = {
     enable = true;
-    capSysNice = true;
+    capSysNice = false; # https://github.com/NixOS/nixpkgs/issues/351516
+  };
+
+  services.ananicy = {
+    enable = true;
+    package = pkgs.ananicy-cpp;
+    rulesProvider = pkgs.ananicy-cpp;
+    extraRules = [
+      {
+        "name" = "gamescope";
+        "nice" = -20;
+      }
+    ];
   };
 }
